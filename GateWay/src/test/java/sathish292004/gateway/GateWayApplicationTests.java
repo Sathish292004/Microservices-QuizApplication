8d4fba82e6f86452b175be7de81eb7c89661c2bb
Sathish292004 <sathishsince2004@gmail.com>
2026-07-16 14:48:37 +0530

    Initial commit

---
+++ GateWay/src/test/java/sathish292004/gateway/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package sathish292004.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
